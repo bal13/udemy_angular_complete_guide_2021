abd0b7a578b01463bfcb58461681adb4ed99a5da
balazs <szabo.balazs13@gmail.com>
2021-08-02 10:22:07 +0200

    Folytatás

---
+++ ng4-complete-guide/src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+    // Shortcut: ha ilyen módon néz a konstruktor, akkor nem kellenek
+    // külön az attribútumok + beállítások
+    constructor(public name: string, public amount: number) {}
+}
